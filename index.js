d24eb306913b30927a5328eb0e173b6ce3b83fb8
EnisMollaahmed <enis_super02@outlook.com>
2025-03-05 11:43:53 +0200

    corrected version

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const inputField = document.querySelector(".todo-input");
 const buttn = document.querySelector(".add-button");
 const uList = document.querySelector(".ulist");
 
-buttn.addEventListener("click", ()=>{
+buttn.addEventListener("click", (e)=>{
     const lItem = document.createElement("li");
+    console.log(inputField.value);
     lItem.textContent = inputField.value;
     uList.appendChild(lItem);
+    inputField.value = "";
 });
